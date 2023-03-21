46f7317b01fa5686df5ac62d99ea83bc8158e3cc
yonhgo9064 <dydgmldydgh@naver.com>
2023-03-21 22:57:40 +0900

    first commit

---
+++ sp5-chap02/src/main/java/chap02/AppContext.java
@@ -0,0 +1,21 @@
+package chap02;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppContext {
+    @Bean
+    public Greeter greeter(){
+        Greeter g = new Greeter();
+        g.setFormat("%s, 안녕하세요!");
+        return g;
+    }
+
+    @Bean
+    public Greeter greeter1(){
+        Greeter g = new Greeter();
+        g.setFormat("안녕하세요, %s님!");
+        return g;
+    }
+}
